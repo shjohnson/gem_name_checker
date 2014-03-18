a820abdff134b7b15769020df8037905f27d047b
Mal Pinder <mal.pinder@fundingcircle.com>
2014-03-18 15:55:34 +0000

    Small change to name_picker.

--- lib/name_picker.rb
+++ lib/name_picker.rb
@@ -8,12 +8,15 @@ class NamePicker
   end
 
   def response
-    checker = Checker.new(@name)
     if checker.available?
       "That name is available."
     else
       "That name is not available: why not try #{Suggestor.alternatives_for(@name)}"
     end
+  end
+
+  def checker
+    Checker.new(@name)
   end
 
 end
